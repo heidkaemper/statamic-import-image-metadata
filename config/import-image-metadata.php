e6e4592c6a5e912e17f72562e707fdada70e585e
Lars Heidkämper <lars.heidkaemper@gmail.com>
2022-12-18 17:15:19 +0100

    config comments

--- config/import-image-metadata.php
+++ config/import-image-metadata.php
@@ -7,7 +7,12 @@ return [
     | Fields
     |--------------------------------------------------------------------------
     |
-    | ...
+    | Map your asset fields to metadata tags.
+    |
+    | The keys are field handles of the asset container blueprint. The values
+    | are a comma separated list of EXIF and IPTC tags.
+    |
+    | Example: 'your_field' => 'iptc.author, exif.artist, iptc.2#110'
     |
     */
 
@@ -22,18 +27,21 @@ return [
     | Reupload
     |--------------------------------------------------------------------------
     |
-    | ...
+    | When an image is re-uploaded, the metadata will be overwritten with
+    | those of the new image. This can be disabled by setting
+    | reupload to 'false'.
     |
     */
 
-    'reupload' => false,
+    'reupload' => true,
 
     /*
     |--------------------------------------------------------------------------
     | Queue
     |--------------------------------------------------------------------------
     |
-    | ...
+    | If the import metadata job is being queued, specify the name of the
+    | target queue. This falls back to the 'default' queue.
     |
     */
 
