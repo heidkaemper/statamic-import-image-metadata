e2bcd52adaded541ea875a8cb545e41b4abf5091
Lars Heidkämper <lars.heidkaemper@gmail.com>
2024-05-10 22:29:50 +0200

    prepare feature tests

---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+use Heidkaemper\ImportImageMetadata\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,15 @@ class TestCase extends OrchestraTestCase
     protected function getPackageProviders($app): array
     {
         return [
+            \Statamic\Providers\StatamicServiceProvider::class,
             \Heidkaemper\ImportImageMetadata\ServiceProvider::class,
         ];
+    }
+
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $app['config']->set('statamic.users.repository', 'file');
     }
 }
